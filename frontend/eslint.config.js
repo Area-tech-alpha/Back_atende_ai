cb54a596f049583112ed7d8f97fc2071074d3ccb
PratadoYoutube <e.deanmb@gmail.com>
2025-08-08 20:00:22 -0300

    fix: corrigi eslint definitivo

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,11 +1,11 @@
-const globals = require("globals");
-const js = require("@eslint/js");
-const tsParser = require("@typescript-eslint/parser");
-const tsPlugin = require("@typescript-eslint/eslint-plugin");
-const reactPlugin = require("eslint-plugin-react");
-const reactHooks = require("eslint-plugin-react-hooks");
+import globals from "globals";
+import js from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import reactPlugin from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
 
-module.exports = [
+export default [
   // Habilita as regras recomendadas do JavaScript (ESLint padrão)
   js.configs.recommended,
   
