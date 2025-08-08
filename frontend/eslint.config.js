c6de3d17029665819fd34e6f4a0313474e8db8f2
PratadoYoutube <e.deanmb@gmail.com>
2025-08-08 19:48:45 -0300

    WIP

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,13 +1,20 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
 
 export default [
   {
+    // Define os arquivos que o ESLint deve verificar
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    // Ignora pastas comuns para evitar problemas de desempenho
+    ignores: ["node_modules/", "dist/", "build/"],
+
     languageOptions: { 
       globals: globals.browser,
-      parser: tseslint.parser,
+      parser: tsParser, 
       parserOptions: {
         ecmaFeatures: {
           jsx: true,
@@ -15,15 +22,19 @@ export default [
         ecmaVersion: "latest",
         sourceType: "module",
       },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules, 
     }
   },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
   pluginReactConfig,
   {
     rules: {
       // Adicione suas regras personalizadas aqui, se necess\u00e1rio
     },
-    // Omitir o eslint-plugin-react-refresh e eslint-plugin-react-hooks para evitar o erro de 'module not found'
   },
 ];
