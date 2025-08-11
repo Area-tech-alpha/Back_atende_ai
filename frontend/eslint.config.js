2eecfc60ed9b1c360d66e92d2a89502a9d3f8e05
PratadoYoutube <e.deanmb@gmail.com>
2025-08-11 11:08:28 -0300

    WIP

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -9,9 +9,10 @@ export default [
   // Adiciona uma regra para ignorar arquivos e pastas
   {
     ignores: ["dist/**/*", "node_modules/**/*"],
-  }, // Habilita as regras recomendadas do JavaScript (ESLint padrão)
-
-  js.configs.recommended, // Habilita as regras recomendadas do TypeScript
+  },
+  // Habilita as regras recomendadas do JavaScript (ESLint padrão)
+  js.configs.recommended,
+  // Habilita as regras recomendadas do TypeScript
   {
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
@@ -26,17 +27,18 @@ export default [
     rules: {
       ...tsPlugin.configs.recommended.rules,
     },
-  }, // Habilita as regras recomendadas do React, seguindo a nova flat config
-
+  },
+  // Habilita as regras recomendadas do React, seguindo a nova flat config
   {
     files: ["**/*.{js,jsx,ts,tsx}"],
-    ...reactPlugin.configs.flat.recommended, // Adiciona o suporte para o novo JSX transform
-    ...reactPlugin.configs.flat["jsx-runtime"], // Configurações compartilhadas
+    ...reactPlugin.configs.flat.recommended,
+    ...reactPlugin.configs.flat["jsx-runtime"],
     settings: {
       react: {
         version: "detect", // Detecta a versão do React automaticamente
       },
-    }, // Configurações de linguagem e plugins
+    },
+    // Configurações de linguagem e plugins
     languageOptions: {
       ...reactPlugin.configs.flat.recommended.languageOptions,
       globals: {
@@ -44,21 +46,24 @@ export default [
         ...globals.node,
       },
       parser: tsParser,
-    }, // Desativa as regras que estavam causando os erros no seu código
-
+    },
+    // Desativa as regras que estavam causando os erros no seu código
     rules: {
+      "no-irregular-whitespace": "off", // Desativa a regra de espaço em branco
       "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/ban-ts-comment": "off",
-      "no-empty": "off", // Regras de React
+      "no-empty": "off",
+      // Regras de React
       "react/jsx-uses-vars": "error",
       "react-hooks/rules-of-hooks": "error",
       "react-hooks/exhaustive-deps": "warn",
       "react/react-in-jsx-scope": "off",
       "no-undef": "error",
     },
-  }, // Configuração para react-hooks
+  },
+  // Configuração para react-hooks
   {
     files: ["**/*.{js,jsx,ts,tsx}"],
     plugins: {
